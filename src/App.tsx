dbea934ed7d070dd07993e17846859da300d2f90
pzajas <zajas.piotr@gmail.com>
2024-08-15 12:13:07 +0200

    Add initial scheduler with hardcoded data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { CustomScheduler } from "./components/CustomScheduler";
+import { getCurrentDate } from "./helpers/dateHelpers";
+
 const App = () => {
-  return <>React Scheduler</>;
+  const schedulerData = [
+    {
+      startDate: "2024-08-15T09:45",
+      endDate: "2024-08-15T11:00",
+      title: "Meeting",
+    },
+  ];
+  const currentDate = getCurrentDate();
+
+  return (
+    <>
+      <CustomScheduler
+        currentDate={currentDate}
+        schedulerData={schedulerData}
+      />
+    </>
+  );
 };
 
 export default App;
